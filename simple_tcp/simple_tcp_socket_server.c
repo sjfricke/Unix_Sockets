bb2e65a518cc221878c42192ed03ed4da27c46f9
sjfricke <sjfricke@wisc.edu>
2017-02-14 23:22:47 -0600

    almost working TCP
    need to fix blocking issue

--- simple_tcp/simple_tcp_socket_server.c
+++ simple_tcp/simple_tcp_socket_server.c
@@ -58,8 +58,9 @@ int main(int argc, char *argv[]) {
   struct sockaddr_in dest; // socket info about the machine connecting to us
   struct sockaddr_in serv; // socket info about our server
   int mySocket;            // socket used to listen for incoming connections
+  int consocket;           // used to hold status of connect to socket 
   socklen_t socksize = sizeof(struct sockaddr_in);
-
+  
   // allocates size for receive message
   receiveMsg = malloc(sizeof(char) * MSG_SIZE); // max chars for sending data 
   if (receiveMsg == NULL) { error("ERROR: receiveMsg allocation"); }
@@ -92,37 +93,55 @@ int main(int argc, char *argv[]) {
   // start listening, allowing a queue of up to 1 pending connection
   listen(mySocket, 1);
   printf("Socket Listening on port %d!\n", port);
-  int consocket = accept(mySocket, (struct sockaddr *)&dest, &socksize); //blocking for response
 
-  // main loop to wait for a request
-  while(consocket) {
+  // prevents daemon from closing on a closed client
+  signal(SIGPIPE, SIG_IGN);
+  
+  for (;;) { // keeps daemon running forever
+
+    // blocks until a TCP handshake is made
+    consocket = accept(mySocket, (struct sockaddr *)&dest, &socksize);
 
     // a connection has been been received can see the client IP
     printf("Incoming connection from %s \n", inet_ntoa(dest.sin_addr));
+    msgSize = 1;
+    
+    // main loop to wait for a request
+    while(consocket && msgSize > 0) {
 
-    // used to get the message of length MSG_SIZE
-    msgSize = recv(consocket, receiveMsg, MSG_SIZE , 0);
-    printf("Client sent the character: %s\n", receiveMsg);
+      printf("recv %d\tstatus %d\tconsocket %d\n", msgSize, status, consocket);
+      
+      // used to get the message of length MSG_SIZE
+      msgSize = recv(consocket, receiveMsg, MSG_SIZE , 0);
+      if (msgSize < 0) { error("ERROR on recv"); }
+      
+      printf("Client sent the character: %s\n", receiveMsg);
 
-    // adds to message counter if one of the accepted letters
-    if (receiveMsg[0] == counter.a) { 
-      counter.a_count++;
-    } else if (receiveMsg[0] == counter.b) {
-      counter.b_count++;
-    } else if (receiveMsg[0] == counter.c) {
-      counter.c_count++;
-    }
+      // adds to message counter if one of the accepted letters
+      if (receiveMsg[0] == counter.a) { 
+	counter.a_count++;
+      } else if (receiveMsg[0] == counter.b) {
+	counter.b_count++;
+      } else if (receiveMsg[0] == counter.c) {
+	counter.c_count++;
+      }
 	
-    printf("Current Count\n\tA: %d\n\tB: %d\n\tC: %d\n", counter.a_count, counter.b_count, counter.c_count);
+      printf("Current Count\n\tA: %d\n\tB: %d\n\tC: %d\n", counter.a_count, counter.b_count, counter.c_count);
 
-    memset(receiveMsg,0, msgSize); //clears receive message
+      //clears receive message
+      bzero(receiveMsg, MSG_SIZE);
 
-    send(consocket, returnMsg, strlen(returnMsg), 0); //sends back ack message
+      //sends back response message
+      status = send(consocket, returnMsg, strlen(returnMsg), 0);
+      if (status < 0) {
+	consocket = 0; // ends for loop
+	close(consocket); // ends current TCP connection
+	puts("client dropped connection");
+      }
+      
+    } //connection while loop
 
-    close(consocket); //ends current TCP connection
-
-    consocket = accept(mySocket, (struct sockaddr *)&dest, &socksize); //blocks for next response
-  }
+  } // forever for loop
 
   // clean up on close
   printf("\nClosing Socket\n");
